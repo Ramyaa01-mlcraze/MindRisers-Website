7280e2aad5f6e30267f0231758be2bf8c0938c79
pavithraseerala <seeralapavithra123@gmail.com>
2021-09-27 12:02:30 +0530

    pages inclusion commit

--- webapp/basewebsite/urls.py
+++ webapp/basewebsite/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    path('',views.baseHome,name='baseHome'),
+    path('aboutus',views.aboutus,name='aboutus'),
+    path('register',views.register,name='register'),
     path('login',views.login,name='login')
 ]
--- webapp/basewebsite/views.py
+++ webapp/basewebsite/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def baseHome(request):
+    return render(request,'baseHome.html')
+def aboutus(request):
+    return render(request,'aboutus.html')
+def register(request):
+    return render(request,'register.html')
 def login(request):
     return render(request,'login.html')
